23cf0d378c238ff8c5ac60b1bfadf49ef113af0f
Rosen Yordanov <roskata182003@gmail.com>
2022-08-10 20:27:22 +0300

    Upload Mid Exam Preparation

---
+++ Mid Exams Preparations/01. Programming Fundamentals/Computer Store/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double totalSumWithoutTaxes = 0;
+            bool isSpecial = false;
+            while (command != "special" && command != "regular")
+            {
+                double price = double.Parse(command);
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    totalSumWithoutTaxes += price;
+                }
+                command = Console.ReadLine();
+                if (command == "special")
+                {
+                    isSpecial = true;
+                    break;
+                }
+            }
+            double totalSum = totalSumWithoutTaxes + (totalSumWithoutTaxes * 20) / 100;
+            if (totalSum == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+            double taxes = totalSum - totalSumWithoutTaxes;
+            if (isSpecial)
+            {
+                totalSum = totalSum - (totalSum * 10) / 100;
+            }
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {totalSumWithoutTaxes:f2}$");
+            Console.WriteLine($"Taxes: {(taxes):f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalSum:f2}$");
+        }
+    }
+}
---
+++ Mid Exams Preparations/01. Programming Fundamentals/The Lift/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int capacity = 4;
+            int countPeople = int.Parse(Console.ReadLine());
+            List<int> wagons = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                int peopleToWagon = capacity - wagons[i];
+                if (capacity - wagons[i] >= peopleToWagon)
+                {
+                    if (countPeople < peopleToWagon)
+                    {
+                        wagons[i] += countPeople;
+                        countPeople -= countPeople;
+                    }
+                    else
+                    {
+                        wagons[i] += peopleToWagon;
+                        countPeople -= peopleToWagon;
+                    }
+                }
+            }
+            CheckWagons(wagons, countPeople);
+
+            PrintResult(wagons);
+
+        }
+
+
+        static void CheckWagons(List<int> wagons, int countPeople)
+        {
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                if (wagons[i] < 4)
+                {
+                    Console.WriteLine("The lift has empty spots!");
+                    break;
+                }
+                if (countPeople > 0)
+                {
+                    Console.WriteLine($"There isn't enough space! {countPeople} people in a queue!");
+                    break;
+                }
+            }
+        }
+        static void PrintResult(List<int> wagons)
+        {
+            foreach (var item in wagons)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
