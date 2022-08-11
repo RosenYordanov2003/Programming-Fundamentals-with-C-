42a4cc6c3a00f153a31d4df3b2c61c8d4e0962bc
Rosen Yordanov <roskata182003@gmail.com>
2022-08-11 14:02:33 +0300

    Upload more Mid Exams

---
+++ Mid Exams Preparations/02. Programming Fundamentals Mid Exam/Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
