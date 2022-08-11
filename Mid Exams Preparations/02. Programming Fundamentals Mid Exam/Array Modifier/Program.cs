a153abbce1d83fb0c4fdc25d96f7b7b2fe5f597e
Rosen Yordanov <roskata182003@gmail.com>
2022-08-11 14:03:43 +0300

    Add exercises

---
+++ Mid Exams Preparations/02. Programming Fundamentals Mid Exam/Array Modifier/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Array_Modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] tokens = command.Split(" ").ToArray();
+                string operation = tokens[0];
+                if (operation == "swap")
+                {
+                    int index1 = int.Parse(tokens[1]);
+                    int index2 = int.Parse(tokens[2]);
+                    SwapIndex(numbers, index1, index2);
+                }
+                else if (operation == "multiply")
+                {
+                    int indexToMultiply = int.Parse(tokens[1]);
+                    int secondIndexToMultiply = int.Parse(tokens[2]);
+                    MultiplyMethod(numbers, indexToMultiply, secondIndexToMultiply);
+                }
+                else if (operation == "decrease")
+                {
+                    DecreaseElements(numbers);
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        static void DecreaseElements(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i]--;
+            }
+        }
+
+        static void MultiplyMethod(List<int> numbers, int indexToMultiply, int secondIndexToMultiply)
+        {
+            numbers[indexToMultiply] = numbers[indexToMultiply] * numbers[secondIndexToMultiply];
+        }
+
+        static void SwapIndex(List<int> numbers, int index1, int index2)
+        {
+            int tempFirstIndex = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = tempFirstIndex;
+        }
+    }
+}
---
+++ Mid Exams Preparations/02. Programming Fundamentals Mid Exam/SoftUni Reception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeerStudents = int.Parse(Console.ReadLine());
+            int secondEmployeerStudents = int.Parse(Console.ReadLine());
+            int thirdEmployeerStudents = int.Parse(Console.ReadLine());
+            int totalCountStudents = int.Parse(Console.ReadLine());
+            int countHours = 0;
+            int totalHandleStudentsPerHour = firstEmployeerStudents + secondEmployeerStudents + thirdEmployeerStudents;
+            int currentStudentsHandle = 0;
+            while (totalCountStudents > 0)
+            {
+                countHours++;
+                if (countHours % 4 == 0)
+                {
+                    continue;
+                }
+                currentStudentsHandle += totalHandleStudentsPerHour;
+                totalCountStudents -= totalHandleStudentsPerHour;
+
+            }
+            Console.WriteLine($"Time needed: {countHours}h.");
+        }
+    }
+}
