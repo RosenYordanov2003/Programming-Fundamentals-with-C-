416138c153ce34b81a98f4b38ad9900375519a88
Rosen Yordanov <roskata182003@gmail.com>
2022-08-12 20:19:30 +0300

    Upload Programming Fundamentals 03 Exam

---
+++ Mid Exams Preparations/03. Programming Fundamentals Mid Exam/Counter-Strike/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03._Programming_Fundamentals_Mid_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string comand = string.Empty;
+            int countWonBattles = 0;
+            bool dontEnoughEnergy = false;
+            while ((comand = Console.ReadLine()) != "End of battle")
+            {
+                int distanceToEnemy = int.Parse(comand);
+                if (distanceToEnemy > initialEnergy)
+                {
+                    dontEnoughEnergy = true;
+                    Console.WriteLine($"Not enough energy! Game ends with {countWonBattles} won battles and {initialEnergy} energy");
+                    break;
+                }
+                else
+                {
+                    countWonBattles++;
+                    initialEnergy -= distanceToEnemy;
+                }
+                if (countWonBattles % 3 == 0)
+                {
+                    initialEnergy += countWonBattles;
+                }
+            }
+            if (!dontEnoughEnergy)
+            {
+                Console.WriteLine($"Won battles: {countWonBattles}. Energy left: {initialEnergy}");
+            }
+        }
+    }
+}
---
+++ Mid Exams Preparations/03. Programming Fundamentals Mid Exam/Shoot for the Win/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Shoot_for_the_Win
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            string command = string.Empty;
+            int counterShootTargets = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int indexToShoot = int.Parse(command);
+                if (indexToShoot >= 0 && indexToShoot < targets.Length)
+                {
+                    int previusTargetBeforeShoot = targets[indexToShoot];
+                    targets[indexToShoot] = -1;
+                    counterShootTargets++;
+                    ReduceOrIncrease(targets, previusTargetBeforeShoot);
+                }
+            }
+            Console.WriteLine($"Shot targets: {counterShootTargets} -> {string.Join(" ", targets)}");
+        }
+
+        static void ReduceOrIncrease(int[] targets, int previusTargetBeforeShoot)
+        {
+            int power = previusTargetBeforeShoot;
+            for (int i = 0; i < targets.Length; i++)
+            {
+                int currentTarget = targets[i];
+                if (currentTarget > power && currentTarget != -1)
+                {
+                    targets[i] -= power;
+                }
+                else if (currentTarget <= power && currentTarget != -1)
+                {
+                    targets[i] += power;
+                }
+            }
+        }
+    }
+}
