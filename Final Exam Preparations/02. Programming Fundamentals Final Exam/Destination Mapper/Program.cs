543c557e6a1da69b8832b9f57ac0177f7804f1ac
Rosen Yordanov <roskata182003@gmail.com>
2022-08-17 10:31:58 +0300

    Upload Programming Fundamentals Final Exam 02

---
+++ Final Exam Preparations/02. Programming Fundamentals Final Exam/Destination Mapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(\={1}|\/{1})(?<destination>[A-Z]{1}[A-Za-z]{2,})\1";
+            Regex regex = new Regex(pattern);
+            int sum = 0;
+            List<string> validLocations = new List<string>();
+            MatchCollection loctions = regex.Matches(input);
+            foreach (Match match in loctions)
+            {
+                string currentDestination = match.Groups["destination"].Value;
+                sum += currentDestination.Length;
+                validLocations.Add(currentDestination);
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", validLocations)}");
+            Console.WriteLine($"Travel Points: {sum}");
+        }
+    }
+}
---
+++ Final Exam Preparations/02. Programming Fundamentals Final Exam/World Tour/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace World_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+
+                string[] tokens = command.Split(':');
+                string action = tokens[0];
+                if (action == "Add Stop")
+                {
+                    int index = int.Parse(tokens[1]);
+                    string text = tokens[2];
+                    if (index >= 0 && index < input.Length)
+                    {
+                        input = input.Insert(index, text);
+                    }
+                }
+                else if (action == "Remove Stop")
+                {
+                    int startIndex = int.Parse(tokens[1]);
+                    int endIndex = int.Parse(tokens[2]);
+                    if ((startIndex >= 0 && startIndex < input.Length) && (endIndex >= 0 && endIndex < input.Length))
+                    {
+                        input = input.Remove(startIndex, endIndex - startIndex + 1);
+                    }
+
+                }
+                else if (action == "Switch")
+                {
+                    string oldText = tokens[1];
+                    string newText = tokens[2];
+                    if (input.Contains(oldText))
+                    {
+                        input = input.Replace(oldText, newText);
+                    }
+
+                }
+
+                Console.WriteLine(input);
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {input}");
+        }
+    }
+}
